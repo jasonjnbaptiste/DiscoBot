1bbdc5c7883c1a1ccc2b5c6961aaa257c0765e79
Jason Jn Baptiste <jnbaptiste.j@northeastern.edu>
2025-06-25 18:40:48 -0400

    DiscoBot V1

---
+++ Commands/music/play.js
@@ -0,0 +1,36 @@
+const { createAudioResource, joinVoiceChannel, createAudioPlayer, AudioPlayerStatus, StreamType } = require('@discordjs/voice');
+const { SlashCommandBuilder } = require('discord.js');
+const ytdl = require('ytdl-core');
+
+module.exports = {
+  data : new SlashCommandBuilder()
+    .setName('play')
+    .setDescription('Play a url.')
+    .addStringOption(option =>
+      option.setName('url')
+        .setDescription('The url to play')
+        .setRequired(true)),
+  async execute(interaction) {
+    const url = interaction.options.getString('url');
+    console.log(url);
+    const stream = ytdl(url, { filter: 'audioonly' });
+    const resource = createAudioResource(stream, { inputType: StreamType.Arbitrary });
+
+    const voiceChannel = interaction.member.voice.channel;
+    if (!voiceChannel) return interaction.reply('Join a voice channel first!');
+
+    const connection = joinVoiceChannel({
+      channelId: voiceChannel.id,
+      guildId: voiceChannel.guild.id,
+      adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+      selfDeaf: false,
+    });
+
+    const player = createAudioPlayer();
+    player.play(resource);
+    connection.subscribe(player);
+
+    player.on(AudioPlayerStatus.Idle, () => connection.destroy());
+    await interaction.reply(`Now play ${url}, requested by ${interaction.user.username}`);
+  },
+};
--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const { token } = require('./config.json');
 
 // Create a new client instance
 const client = new Client({ intents: [
-  GatewayIntentBits.Guilds] });
+  GatewayIntentBits.Guilds,
+  GatewayIntentBits.GuildVoiceStates,
+  GatewayIntentBits.GuildMessages,
+  GatewayIntentBits.MessageContent,
+] });
 
 client.commands = new Collection();
 
